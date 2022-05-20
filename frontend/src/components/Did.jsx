f6b02b58bc7a573a4c75835d4fb8f03f83921a73
Bon Nicolai Mercado <bonhokage06@gmail.com>
2022-05-21 04:57:41 +0800

    handle did for frontend

--- frontend/src/components/Did.jsx
+++ frontend/src/components/Did.jsx
@@ -1,10 +1,26 @@
+import { useTransition } from "react"
+import useSetupStore  from "../store/setUp"
 import DidStyle from "../assets/css/did.module.css"
 import ButtonStyle from "../assets/css/button.module.css"
-import useStepsStore from "../store/steps"
 
-export default function Did(){
-    const decreaseSteps = useStepsStore(state => state.decreaseSteps)
-    const increaseSteps = useStepsStore(state => state.increaseSteps)
+export default function Did({increaseSteps,decreaseSteps}){
+    const [pending,setTransition] = useTransition()
+    const did = useSetupStore(state => state.did)
+    const processDid = useSetupStore (state => state.processDid)    
+    const hasDid = did.id?.length > 0
+    const handleDidClick = () => {
+        setTransition(()=>{
+            processDid()
+        })
+    }
+    const handleNextOrSkipClick = () =>{
+        if(hasDid){
+            increaseSteps()
+            increaseSteps()
+            return;
+        }
+        increaseSteps()
+    }
     return <div className={DidStyle['app-did']}>
         <h1>Sign-in with DID</h1>
         <p>
@@ -12,10 +28,22 @@ export default function Did(){
             <br/>
             With DIDs you can share with any DApps.    
         </p>
-        <button className={`btn btn-secondary ${ButtonStyle['essentials']}`}>Connect Essentials</button>
+        <button 
+        className={`btn btn-secondary ${ButtonStyle['essentials']} ${hasDid ? ButtonStyle['disabled']:''}`} 
+        onClick={handleDidClick}>
+            {hasDid ? "Connected":"Connect Essentials"}
+        </button>
         <div className={ButtonStyle['group']}>
-            <button className={`btn btn-primary ${ButtonStyle['prev']}`} onClick={decreaseSteps}>Previous</button>
-            <button className={`btn btn-primary ${ButtonStyle['skip']}`} onClick={increaseSteps}>Skip</button>            
+            <button 
+            className={`btn btn-primary ${ButtonStyle['prev']}`} 
+            onClick={decreaseSteps}>
+                Previous
+            </button>
+            <button 
+            className={`btn btn-primary ${ButtonStyle['skip']}`} 
+            onClick={handleNextOrSkipClick}>
+                {hasDid ? "Next":"Skip"}
+            </button>            
         </div>
     </div>
 }
--- frontend/src/store/setUp.js
+++ frontend/src/store/setUp.js
@@ -1,10 +1,11 @@
 import create from "zustand";
 
-
 const useSetupStore = create(set => ({
     isetUpCompleted:false,    
     isExternalStorageConnected:false,
+    did:{},
     processSetUp: () => set(state => ({ isetUpCompleted: true })),
+    processDid: ()=> set(state => ({ did: {id:"testing"} })),
     toggleExternalStorageConnected: () => set(state => ({ isExternalStorageConnected: true })),
 }))
 
