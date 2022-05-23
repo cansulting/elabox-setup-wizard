3451947195b62ce0f1e7431cb823c31d9b29703a
Jhoemar Pagao <jhoemar_pagao@yahoo.com>
2022-05-23 12:19:16 +0000

    added test script

--- backend/main_test.go
+++ backend/main_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"elabox-setup/backend/setup_did"
+	"elabox-setup/backend/setup_keystore"
 	"elabox-setup/backend/setup_usb"
 	"testing"
 	"time"
@@ -19,5 +21,30 @@ func Test_Usb(t *testing.T) {
 	logger.Init("ela.setup.test")
 	if err := setup_usb.SetupAtHome("sdb", "/home/elabox2"); err != nil {
 		t.Error(err.Error())
+	}
+}
+
+// expected an issue while generating keystore
+func Test_KeystoreGen_Failure(t *testing.T) {
+	logger.Init("ela.setup.test")
+	if err := setup_keystore.Setup("erwr"); err == nil {
+		t.Error("expected an error for password but nothing was recieved")
+	}
+}
+
+func Test_KeystoreGen_Success(t *testing.T) {
+	logger.Init("ela.setup.test")
+	if wassetup := setup_keystore.WasSetup(); wassetup {
+		t.Log("keystore was already setup, process skipped")
+		return
+	}
+	if err := setup_keystore.Setup("helloworld"); err != nil {
+		t.Error(err)
+	}
+}
+func Test_DidSetup(t *testing.T) {
+	logger.Init("ela.setup.test")
+	if err := setup_did.Setup(`{"holder":"this is a sample did"}`); err != nil {
+		t.Error(err)
 	}
 }
