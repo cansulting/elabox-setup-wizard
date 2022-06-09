6d5cd8c08aa751062ef87e8833159befad34cc20
Bon Nicolai Mercado <bonhokage06@gmail.com>
2022-06-09 16:12:42 +0000

    redirect to ela.companion

--- frontend/src/components/Finished.jsx
+++ frontend/src/components/Finished.jsx
@@ -3,7 +3,7 @@ import ButtonStyle from "../assets/css/button.module.css"
 
 export default function Finished(){
     const redirect = () => {
-        window.location.href = window.location.origin
+        window.location.href = "/ela.companion" 
     }
     return <div className={FinishedStyle['app-finished']}>
         <h1>Congratulations, you're all setup!</h1>
