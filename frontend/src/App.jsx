1496418c86106e759bddca3cbb52da98fa4029ac
Bon Nicolai Mercado <bonhokage06@gmail.com>
2022-07-14 12:20:49 +0000

    update handling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
   return (
     <div className="App">
       <Helmet>
-        {isOnionUrl && <>
+        {!isOnionUrl && <>
           <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
           <meta http-equiv="Pragma" content="no-cache" />
           <meta http-equiv="Expires" content="0" />        
